177fc83e2f48cf66eed85d63da91a938912c800e
Jsmartee <jsmartee23@gmail.com>
2021-02-07 19:57:20 -0700

    default reports for vanilla settings

--- js/main.js
+++ js/main.js
@@ -19,7 +19,7 @@ function init() {
 function update() {
     var updates = document.getElementsByClassName('update');
     for(var i = 0; i < updates.length; i++) {
-        updates[i].innerHTML = "Last Updated 2/6/21";
+        updates[i].innerHTML = "Last Updated 2/7/21";
     }
 }
 
